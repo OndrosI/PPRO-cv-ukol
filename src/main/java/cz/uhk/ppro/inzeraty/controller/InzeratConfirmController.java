214dc813506660e44b9db8c86833a67ef0bbe8ca
Ondřej Kašpar <ondrej.kaspar@tmapy.cz>
2021-10-14 17:23:41 +0200

    del imports

--- src/main/java/cz/uhk/ppro/inzeraty/controller/InzeratConfirmController.java
+++ src/main/java/cz/uhk/ppro/inzeraty/controller/InzeratConfirmController.java
@@ -1,8 +1,6 @@
 package cz.uhk.ppro.inzeraty.controller;
 
 import cz.uhk.ppro.inzeraty.model.Inzerat;
-import cz.uhk.ppro.inzeraty.sluzby.PametoveUlozisteInzeratu;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
